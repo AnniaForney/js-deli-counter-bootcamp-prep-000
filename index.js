a157f9085c9f434804166e8f54e364f05cbb5231
Annia Forney <anniaforney@gmail.com>
2019-03-27 11:41:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+}
+
 function currentLine(line) {
   if (!line.length) {
     return "The line is currently empty."
@@ -10,18 +24,4 @@ function currentLine(line) {
   }
 
   return `The line is currently: ${numbersAndNames.join(', ')}`
-};
-
-function nowServing(line) {
-  if (!line.length) {
-    return "There is nobody waiting to be served!"
-  }
-
-  return `Currently serving ${line.shift()}.`
-};
-
-function takeANumber(line, name) {
-  line.push(name)
-
-  return `Welcome, ${name}. You are number ${line.length} in line.`
-};
+} 
