41cd8fbf775acca424fd977550bedb3a69ae6a88
Annia Forney <anniaforney@gmail.com>
2019-03-27 11:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function takeANumber (current,line){
-  return `'Welcome, ${name}.'` `'You are number 1 in line.'`;
-}
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
 
-function nowServing(line){
-  if (!line.length)
-  return "There is nobody waiting to be served!";
-}
+  const numbersAndNames = []
 
-function currentLine(line){
-  if(!line.length)
-  return "The line is currently empty.";
-}
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!"
+  }
+
+  return `Currently serving ${line.shift()}.`
+};
+
+function takeANumber(line, name) {
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+};
